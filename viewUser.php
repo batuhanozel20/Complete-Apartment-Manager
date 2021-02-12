a524ca2542db0758085cbb85bac3e9217b853444
Batuhan Özel <72559805+batuhanozel20@users.noreply.github.com>
2021-02-12 15:23:08 +0300

    Update viewUser.php

--- viewUser.php
+++ viewUser.php
@@ -5,25 +5,44 @@ include("authenticate.php");
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
- body{ 
-  background-image:url('b.jpg');
-  background-repeat:no-repeat;
-  background-size: cover;
+ body{
+	 background-image:url(https://s3.envato.com/files/243754334/primag.jpg);
+	 background-repeat:no-repeat;
+	 background-size:cover;
+	 width:100%;
+	 height:100vh;
+	 overflow:auto;
+	 
 }
-.button {
-  background-color: #949292; 
-  border: none;
-  color: white;
-  padding: 15px 50px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  }
-  
+th {
+    background-color:#FFB900;
+    
+} 
 
+  h2{
+   color:white 
+  }
+
+  .btn.btn-warning:hover {
+    box-shadow: 2px 1px 2px 3px #99ccff;
+	background:#5900a6;
+	color:#fff;
+ 
+	transition: background-color 1.15s ease-in-out,border-color 1.15s ease-in-out,box-shadow 1.15s ease-in-out;
+	
+}	 
+
+.btn{
+  width: 163px;
+  background-color: orange;
+  color:black;
+}
 </style>
 <meta charset="utf-8">
 <title> Registered Users</title>
@@ -43,7 +62,7 @@ include("authenticate.php");
 <th><strong>E-Mail</strong></th>
 <th><strong>Phone Number</strong></th>
 <th><strong>Door Numberr</strong></th>
-<th><strong>Is Admin</strong></th>
+
 
 
 </tr>
@@ -52,7 +71,7 @@ include("authenticate.php");
 
 <?php
 $count=1;
-$sel_query="SELECT * FROM usertable ORDER BY userID DESC;";
+$sel_query="SELECT * FROM users ORDER BY userID ASC;";
 $result = mysqli_query($conn,$sel_query);
 while($row = mysqli_fetch_assoc($result)) { ?>
 <tr><td align="center"><?php echo $count; ?></td>
@@ -63,7 +82,7 @@ while($row = mysqli_fetch_assoc($result)) { ?>
 <td align="center"><?php echo $row["eMail"]; ?></td>
 <td align="center"><?php echo $row["phoneNo"]; ?></td>
 <td align="center"><?php echo $row["doorNumber"]; ?></td>
-<td align="center"><?php echo $row["isAdmin"]; ?></td>
+
 
 <td align="center">
 <a href="deleteUser.php?id=<?php echo $row["userID"]; ?>">Delete</a>
@@ -72,11 +91,11 @@ while($row = mysqli_fetch_assoc($result)) { ?>
 <?php $count++; } ?>
 </tbody>
 </table>
-
+<br>
 <div style="text-align:center;">
-<p><a href="web.php" class="button">Website</a> 
- <a href="register.php" class="button">Add New</a> 
- <a href="login.php" class="button">Logout</a></p>
+<p><a href="web.php" class="btn btn-warning">Website</a> 
+ <a href="register.php" class="btn btn-warning">Add New</a> 
+ <a href="login.php" class="btn btn-warning">Logout</a></p>
 </div>
 </div>
 </body>
